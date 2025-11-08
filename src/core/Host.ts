193ad768c8459d84db5cdea780cc1e09dbc5ca56
André Francisco <af@neutral.pt>
2025-11-08 07:03:29 +0000

    chore: continue refactor

--- src/core/Host.ts
+++ src/core/Host.ts
@@ -1,9 +1,8 @@
 import ReactReconciler from 'react-reconciler';
-import { Reconciler, type Widget } from '@cruciblehq/ui';
+import { Reconciler, createElement, type Widget } from '@cruciblehq/ui';
 import type { WebRenderer } from "./WebRenderer";
 import type { HostContext } from './HostContext';
 import type { HostContainer } from './HostContainer';
-import { HostAPI } from './HostAPI';
 export type Manifest = {
     widgets: Array<{
         name: string;
@@ -16,16 +15,16 @@ export class Host {
     private readonly renderer: WebRenderer;
     private readonly manifest: Manifest;
 
-    private readonly reconciler: Reconciler<HostContainer, HostContext>;
-    private readonly reactReconciler: ReactReconciler.ReconcilerInstance;
+    private reconciler: Reconciler<HostContainer, HostContext>;
+    private reactReconciler: ReactReconciler.ReconcilerInstance;
     private root: ReturnType<typeof this.reactReconciler.createContainer>;
 
     constructor(renderer: WebRenderer, manifest: Manifest) {
         this.renderer = renderer;
         this.manifest = manifest;
 
-        this.reconciler = new Reconciler(this.renderer);
-        this.reactReconciler = ReactReconciler(this.reconciler.hostConfig);
+        this.reconciler = null as unknown as Reconciler<HostContainer, HostContext>;
+        this.reactReconciler = null as unknown as ReactReconciler.ReconcilerInstance;
     }
 
     public async render(container: HTMLElement): Promise<void> {
@@ -34,13 +33,19 @@ export class Host {
             this.manifest.widgets.map(async (widget) => {
                 const module = await import(widget.path) as { default: Widget };
                 const Widget = module.default;
-                return Widget(new HostAPI());
+                return Widget;
             })
         );
 
+        this.reconciler = new Reconciler(this.renderer);
+        this.reactReconciler = ReactReconciler(this.reconciler.hostConfig);
+
         this.root = this.reactReconciler.createContainer(container, 0, false, '', null) as ReactReconciler.ReconcilerInstance;
 
-        this.reactReconciler.updateContainer(widgetElements[0], this.root, null, () => {
+        const element = createElement(widgetElements[0], { api: null });
+
+
+        this.reactReconciler.updateContainer(element, this.root, null, () => {
             console.warn('Did render widget');
         });
     }
